4a3197cc728e25ec818fc144fd0cde589e52b716
unknown <Administrator@SKCC16N00081.SKCC.NET>
2021-02-26 16:40:30 +0900

    first commit

---
+++ src/main/java/com/example/demo/A2CAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2CAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A2CAppApplication.class, args);
+	}
+
+}
